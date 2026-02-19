f51d3668221d3afef4ef06c1ab5198be92827ce6
Fourk <moksh.bisht@icloud.com>
2026-02-19 21:49:02 +0530

    Initial Commit

---
+++ PulsePlayerUI/PulsePlayerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsePlayerUIApp.swift
+//  PulsePlayerUI
+//
+//  Created by Moksh Bisht on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct PulsePlayerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
